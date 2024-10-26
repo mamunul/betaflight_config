0101116a5598542186bb03da3c8d514d1a4b56e5
Mamunul Mazid <mamunul.mazid@gmail.com>
2024-10-26 15:57:01 +0600

    xetoz: fix 8MB flash was not working for stm32f411 black pill

--- configs/XETOZF411D1/config.h
+++ configs/XETOZF411D1/config.h
@@ -21,10 +21,10 @@
 
 #pragma once
 
-#define FC_TARGET_MCU     STM32F411
+#define FC_TARGET_MCU STM32F411
 
-#define BOARD_NAME        XETOZ_F411D1
-#define MANUFACTURER_ID   XETOZ
+#define BOARD_NAME XETOZ_F411D1
+#define MANUFACTURER_ID XETOZ
 
 #define USE_GYRO
 #define USE_GYRO_SPI_MPU9250
@@ -48,52 +48,50 @@
 #define USE_MAG_HMC5883
 
 #define USE_FLASH
-// #define USE_FLASH_W25M512
-// #define USE_FLASH_W25Q128FV
-#define USE_FLASH_W25Q64
+#define USE_FLASH_W25Q128FV
 
-#define BEEPER_PIN           PB2
-#define LED0_PIN             PA13
-#define LED1_PIN             PA14
+#define BEEPER_PIN PB2
+#define LED0_PIN PA13
+#define LED1_PIN PA14
 
-#define MOTOR1_PIN           PB4
-#define MOTOR2_PIN           PB5
-#define MOTOR3_PIN           PB6
-#define MOTOR4_PIN           PB7
+#define MOTOR1_PIN PB4
+#define MOTOR2_PIN PB5
+#define MOTOR3_PIN PB6
+#define MOTOR4_PIN PB7
 
-#define UART1_TX_PIN         PA9
-#define UART1_RX_PIN         PA10
+#define UART1_TX_PIN PA9
+#define UART1_RX_PIN PA10
 
-#define UART2_TX_PIN         PA2
-#define UART2_RX_PIN         PA3
+#define UART2_TX_PIN PA2
+#define UART2_RX_PIN PA3
 
-#define I2C1_SCL_PIN         PB8
-#define I2C1_SDA_PIN         PB9
+#define I2C1_SCL_PIN PB8
+#define I2C1_SDA_PIN PB9
 
-#define SPI1_SCK_PIN         PA5
-#define SPI1_SDI_PIN         PA6
-#define SPI1_SDO_PIN         PA7
+#define SPI1_SCK_PIN PA5
+#define SPI1_SDI_PIN PA6
+#define SPI1_SDO_PIN PA7
 
-#define SPI2_SCK_PIN         PB13
-#define SPI2_SDI_PIN         PB14
-#define SPI2_SDO_PIN         PB15
+#define SPI2_SCK_PIN PB13
+#define SPI2_SDI_PIN PB14
+#define SPI2_SDO_PIN PB15
 
-#define ADC_VBAT_PIN         PB0
-#define ADC_RSSI_PIN         PB3
-#define ADC_CURR_PIN         PB1
+#define ADC_VBAT_PIN PB0
+#define ADC_RSSI_PIN PB3
+#define ADC_CURR_PIN PB1
 
-#define GYRO_1_EXTI_PIN      PB10
-#define GYRO_1_CS_PIN        PB12
+#define GYRO_1_EXTI_PIN PB10
+#define GYRO_1_CS_PIN PB12
 
-#define TIMER_PIN_MAPPING \
-    TIMER_PIN_MAP( 0, PB4 , 1,  0) \
-    TIMER_PIN_MAP( 1, PB5 , 1,  0) \
-    TIMER_PIN_MAP( 2, PB6 , 1,  0) \
-    TIMER_PIN_MAP( 3, PB7 , 1,  0) 
+#define FLASH_CS_PIN PA4
 
+#define TIMER_PIN_MAPPING       \
+    TIMER_PIN_MAP(0, PB4, 1, 0) \
+    TIMER_PIN_MAP(1, PB5, 1, 0) \
+    TIMER_PIN_MAP(2, PB6, 1, 0) \
+    TIMER_PIN_MAP(3, PB7, 1, 0)
 
-
-#define ADC1_DMA_OPT        1
+#define ADC1_DMA_OPT 1
 
 #define FLASH_SPI_INSTANCE SPI1
 #define DEFAULT_BLACKBOX_DEVICE BLACKBOX_DEVICE_FLASH
